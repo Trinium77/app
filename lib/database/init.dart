5cad99cac0fea340139750d02085e73c832e1fb9
Cyrus Chan <cyruschan1212@gmail.com>
2022-05-15 15:02:42 +0800

    Impl setting

--- lib/database/init.dart
+++ lib/database/init.dart
@@ -1,7 +1,9 @@
+import '../model/global_setting.dart' show globalSettingBoxName;
+
 import 'hive/start.dart' show initHive;
 import 'sql/start.dart' show initSql;
 
 void initDatabases() {
   initSql();
-  initHive(boxName: {"global_setting"});
+  initHive(boxName: {globalSettingBoxName});
 }
