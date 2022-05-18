a139b939448067b16df3549dca60ee9cca31f1e1
Cyrus Chan <cyruschan1212@gmail.com>
2022-05-18 19:18:37 +0800

    Impl homepage

--- lib/ui/page/home.dart
+++ lib/ui/page/home.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 
-class AnitempHomepage extends StatelessWidget {
+class AnitempHomepage extends StatefulWidget {
   const AnitempHomepage({super.key});
 
   @override
-  Widget build(BuildContext context) => Scaffold();
+  State<AnitempHomepage> createState() => _AnitempHomepageState();
+}
+
+class _AnitempHomepageState extends State<AnitempHomepage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      bottomNavigationBar: BottomAppBar(
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <IconButton>[
+                IconButton(onPressed: () {}, icon: const Icon(Icons.settings))
+              ]),
+          shape: const CircularNotchedRectangle()),
+      floatingActionButton:
+          FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked);
 }
