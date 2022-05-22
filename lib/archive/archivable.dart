54aee830a4e1cccb1ba4d5c5f9480053a6c973d9
Cyrus Chan <cyruschan1212@gmail.com>
2022-05-22 14:21:44 +0800

    Add new archivable type

--- lib/archive/archivable.dart
+++ lib/archive/archivable.dart
@@ -1,5 +1,26 @@
+import 'dart:convert';
 import 'dart:typed_data';
 
+import 'package:meta/meta.dart';
+
 abstract class Archivable {
+  const Archivable();
+
   Uint8List toBytes();
+}
+
+mixin JsonBasedArchivable on Archivable {
+  @protected
+  Map<String, dynamic> get jsonData;
+
+  @mustCallSuper
+  @override
+  Uint8List toBytes() {
+    List<int> encoded = utf8.encode(jsonEncode(jsonData));
+
+    return encoded is Uint8List ? encoded : Uint8List.fromList(encoded);
+  }
+
+  static Map<String, dynamic> jbaDecoder(List<int> bytes) =>
+      jsonDecode(utf8.decode(bytes));
 }
