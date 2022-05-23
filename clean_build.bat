3de9bbe6f8b7d59a1efc03a58591cb0ad9076662
Cyrus Chan <cyruschan1212@gmail.com>
2022-05-23 18:57:56 +0800

    Add clean build

---
+++ clean_build.bat
@@ -0,0 +1,4 @@
+@echo off
+
+flutter clean
+flutter get
---
+++ clean_build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# It's better to change shebang (First line comment) to which shell uses currently.
+# Assume you are using sh.
+
+flutter clean
+flutter get
