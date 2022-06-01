dbe3b9cd818961bb6371d2f51401b7b1c90120d3
Cyrus Chan <cyruschan1212@gmail.com>
2022-06-01 13:15:38 +0800

    Use mixin

--- lib/ui/reusable/action_buttons.dart
+++ lib/ui/reusable/action_buttons.dart
@@ -2,6 +2,51 @@ import 'package:flutter/material.dart';
 
 import '../theme/colour.dart';
 
+mixin TwoOptionsActionButtons<B extends ButtonStyleButton> on ActionButtons<B> {
+  VoidCallback? get _accept;
+  VoidCallback? get _discard;
+
+  String _acceptTxt(BuildContext context);
+  String _discardTxt(BuildContext context);
+}
+
+mixin TextButtonBasedTwoOptionsActionButtons
+    on ActionButtons<TextButton>, TwoOptionsActionButtons<TextButton> {
+  @override
+  List<TextButton> _btnOptions(BuildContext context) => <TextButton>[
+        TextButton(
+            onPressed: _accept,
+            child: _btnTextWrapper(Text(_acceptTxt(context),
+                style: btnTextStyle.copyWith(
+                    color: AnitempThemeColourHandler(context).success)))),
+        TextButton(
+            onPressed: _discard,
+            child: _btnTextWrapper(Text(_discardTxt(context),
+                style: btnTextStyle.copyWith(
+                    color: AnitempThemeColourHandler(context).error))))
+      ];
+}
+
+mixin ElevatedButtonBasedTwoOptionsActionButtons
+    on ActionButtons<ElevatedButton>, TwoOptionsActionButtons<ElevatedButton> {
+  @override
+  List<ElevatedButton> _btnOptions(BuildContext context) => <ElevatedButton>[
+        ElevatedButton(
+            onPressed: _accept,
+            child:
+                _btnTextWrapper(Text(_acceptTxt(context), style: btnTextStyle)),
+            style: ButtonStyle(
+                backgroundColor:
+                    AnitempThemeColourHandler(context).mspSuccess)),
+        ElevatedButton(
+            onPressed: _discard,
+            child: _btnTextWrapper(
+                Text(_discardTxt(context), style: btnTextStyle)),
+            style: ButtonStyle(
+                backgroundColor: AnitempThemeColourHandler(context).mspError))
+      ];
+}
+
 abstract class ActionButtons<B extends ButtonStyleButton>
     extends StatelessWidget {
   final EdgeInsetsGeometry btnPadding;
@@ -13,6 +58,8 @@ abstract class ActionButtons<B extends ButtonStyleButton>
       this.btnTextStyle = const TextStyle(fontSize: 18)});
 
   List<B> _btnOptions(BuildContext context);
+
+  Padding _btnTextWrapper(Text txt) => Padding(padding: btnPadding, child: txt);
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +80,8 @@ abstract class ActionButtons<B extends ButtonStyleButton>
   }
 }
 
-class SaveAndDiscardActionButtons extends ActionButtons<ElevatedButton> {
+class SaveAndDiscardActionButtons extends ActionButtons<ElevatedButton>
+    with TwoOptionsActionButtons, ElevatedButtonBasedTwoOptionsActionButtons {
   final VoidCallback? onSave;
   final VoidCallback? onDiscard;
 
@@ -45,27 +93,107 @@ class SaveAndDiscardActionButtons extends ActionButtons<ElevatedButton> {
       super.btnTextStyle});
 
   @override
-  List<ElevatedButton> _btnOptions(BuildContext context) => <ElevatedButton>[
-        ElevatedButton(
-            onPressed: onSave,
-            child: Padding(
-                padding: btnPadding,
-                child: Text(
-                    // TODO: Localize
-                    "Save",
-                    style: btnTextStyle)),
-            style: ButtonStyle(
-                backgroundColor:
-                    AnitempThemeColourHandler(context).mspSuccess)),
-        ElevatedButton(
-            onPressed: onDiscard,
-            child: Padding(
-                padding: btnPadding,
-                child: Text(
-                    // TODO: Localize
-                    "Discard",
-                    style: btnTextStyle)),
-            style: ButtonStyle(
-                backgroundColor: AnitempThemeColourHandler(context).mspError))
-      ];
+  VoidCallback? get _accept => onSave;
+
+  @override
+  String _acceptTxt(BuildContext context) {
+    return "Save";
+  }
+
+  @override
+  VoidCallback? get _discard => onDiscard;
+
+  @override
+  String _discardTxt(BuildContext context) {
+    return "Discard";
+  }
+}
+
+abstract class YesAndNoActionButtons<B extends ButtonStyleButton>
+    implements ActionButtons<B> {
+  VoidCallback? get onYes;
+  VoidCallback? get onNo;
+
+  static YesAndNoActionButtons<TextButton> textButton(
+          {Key? key,
+          required VoidCallback? onYes,
+          required VoidCallback? onNo,
+          EdgeInsetsGeometry btnPadding =
+              const EdgeInsets.symmetric(vertical: 6, horizontal: 10),
+          TextStyle btnTextStyle = const TextStyle(fontSize: 18)}) =>
+      _YesAndNoTextActionButtons(
+          key: key,
+          onYes: onYes,
+          onNo: onNo,
+          btnPadding: btnPadding,
+          btnTextStyle: btnTextStyle);
+
+  static YesAndNoActionButtons<ElevatedButton> elevatedButton(
+          {Key? key,
+          required VoidCallback? onYes,
+          required VoidCallback? onNo,
+          EdgeInsetsGeometry btnPadding =
+              const EdgeInsets.symmetric(vertical: 6, horizontal: 10),
+          TextStyle btnTextStyle = const TextStyle(fontSize: 18)}) =>
+      _YesAndNoElevatedActionButtons(
+          key: key,
+          onYes: onYes,
+          onNo: onNo,
+          btnPadding: btnPadding,
+          btnTextStyle: btnTextStyle);
+}
+
+abstract class _YesAndNoActionButtonsBase<B extends ButtonStyleButton>
+    extends ActionButtons<B>
+    with TwoOptionsActionButtons<B>
+    implements YesAndNoActionButtons<B> {
+  @override
+  final VoidCallback? onNo;
+
+  @override
+  final VoidCallback? onYes;
+
+  _YesAndNoActionButtonsBase(
+      {super.key,
+      required this.onYes,
+      required this.onNo,
+      super.btnPadding,
+      super.btnTextStyle});
+
+  @override
+  VoidCallback? get _accept => onYes;
+
+  @override
+  String _acceptTxt(BuildContext context) {
+    return "Yes";
+  }
+
+  @override
+  VoidCallback? get _discard => onNo;
+
+  @override
+  String _discardTxt(BuildContext context) {
+    return "No";
+  }
+}
+
+class _YesAndNoTextActionButtons extends _YesAndNoActionButtonsBase<TextButton>
+    with TextButtonBasedTwoOptionsActionButtons {
+  _YesAndNoTextActionButtons(
+      {super.key,
+      required super.onYes,
+      required super.onNo,
+      super.btnPadding,
+      super.btnTextStyle});
+}
+
+class _YesAndNoElevatedActionButtons
+    extends _YesAndNoActionButtonsBase<ElevatedButton>
+    with ElevatedButtonBasedTwoOptionsActionButtons {
+  _YesAndNoElevatedActionButtons(
+      {super.key,
+      required super.onYes,
+      required super.onNo,
+      super.btnPadding,
+      super.btnTextStyle});
 }
