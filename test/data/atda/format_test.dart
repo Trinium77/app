ae80cdaf8330712946a2d16af32e0d183edb8068
Cyrus Chan <cyruschan1212@gmail.com>
2022-06-01 11:05:46 +0800

    Sperate ATDA test

--- test/data/atda/format_test.dart
+++ test/data/atda/format_test.dart
@@ -13,42 +13,53 @@ import 'package:anitemp/model/user_setting.dart';
 
 const AnitempCodec _anitemp = AnitempCodec();
 
+get skipTestInCI {
+  Map<String, String> env = Platform.environment;
+  if (env.containsKey("CI")) {
+    return env["TEST_ATDA"] == "1"
+        ? false
+        : "No changes on the format, skipping test to reduce load.";
+  }
+}
+
 void main() {
-  late final Uint8List tb;
-
-  setUpAll(() async {
-    tb = await compute(
-        _anitemp.encode,
-        AnitempCodecData(
-            User(name: "Alice", animal: Animal.human, image: null),
-            <TemperatureRecordNode>[
-              TemperatureRecordNode(
-                  temperature: Celsius(36.4),
-                  recordedAt: DateTime.utc(2022, 3, 1, 12, 34, 31))
-            ],
-            UserSetting.defaultSetting()));
-  });
-
-  test("Verify serialized anitemp data", () async {
-    expect(
-        const ListEquality().equals(tb.sublist(0, 5),
-            Uint8List.fromList(<int>[0x96, 0x99, 0x67, 0x97, 0x60])),
-        isTrue);
-  });
-
-  group("Deserialize data", () {
-    late final AnitempCodecData deserialized;
+  group("Anitemp data archive format test", () {
+    late final Uint8List tb;
 
     setUpAll(() async {
-      deserialized = await compute(_anitemp.decode, tb);
+      tb = await compute(
+          _anitemp.encode,
+          AnitempCodecData(
+              User(name: "Alice", animal: Animal.human, image: null),
+              <TemperatureRecordNode>[
+                TemperatureRecordNode(
+                    temperature: Celsius(36.4),
+                    recordedAt: DateTime.utc(2022, 3, 1, 12, 34, 31))
+              ],
+              UserSetting.defaultSetting()));
     });
 
-    test("user", () {
-      User u = deserialized.user;
-
-      expect(u.name, equals("Alice"));
-      expect(u.animal, equals(Animal.human));
-      expect(u.image, isNull);
+    test("verify serialized anitemp data", () async {
+      expect(
+          const ListEquality().equals(tb.sublist(0, 5),
+              Uint8List.fromList(<int>[0x96, 0x99, 0x67, 0x97, 0x60])),
+          isTrue);
     });
-  });
+
+    group("deserialize data", () {
+      late final AnitempCodecData deserialized;
+
+      setUpAll(() async {
+        deserialized = await compute(_anitemp.decode, tb);
+      });
+
+      test("user", () {
+        User u = deserialized.user;
+
+        expect(u.name, equals("Alice"));
+        expect(u.animal, equals(Animal.human));
+        expect(u.image, isNull);
+      });
+    });
+  }, skip: skipTestInCI);
 }
